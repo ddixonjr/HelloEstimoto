f16afe2fa36222ab8aeb38650ee9e74e9dc96ba4
ddixonjr <ddixon@appivot.com>
2014-08-13 23:18:15 -0400

    Work in progress commit - basic code in place - further work needed to get first experiment working.

--- HelloEstimoto/MYLBeaconListViewController.m
+++ HelloEstimoto/MYLBeaconListViewController.m
@@ -10,11 +10,14 @@
 #import "ESTBeaconManager.h"
 #import "EstBeacon.h"
 
-@interface MYLBeaconListViewController ()
+@interface MYLBeaconListViewController () <ESTBeaconManagerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *beaconListTableView;
 @property (weak, nonatomic) IBOutlet UILabel *beaconQuantityLabel;
 @property (strong, nonatomic) ESTBeaconManager *beaconManager;
+@property (strong, nonatomic) NSArray *beaconList;
+@property (strong, nonatomic) ESTBeaconRegion *region;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 
 @end
 
@@ -24,7 +27,52 @@
 {
     [super viewDidLoad];
 
+    self.beaconManager = [[ESTBeaconManager alloc] init];
+    self.region = [[ESTBeaconRegion alloc] initWithProximityUUID:ESTIMOTE_PROXIMITY_UUID identifier:@"MyBeaconRegion"];
+    [self.beaconManager startRangingBeaconsInRegion:self.region];
+
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(restartRanging) forControlEvents:UIControlEventValueChanged];
+    self.refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"Re-ranging Beacons in Region"];
+    [self.beaconListTableView addSubview:self.refreshControl];
 }
 
+
+- (void)restartRanging
+{
+    [self.beaconManager startRangingBeaconsInRegion:self.region];
+}
+
+//- (void)beaconManager:(ESTBeaconManager *)manager didDiscoverBeacons:(NSArray *)beacons inRegion:(ESTBeaconRegion *)region
+//{
+//    self.beaconList = beacons;
+//    [self.beaconListTableView reloadData];
+//}
+
+- (void)beaconManager:(ESTBeaconManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(ESTBeaconRegion *)region
+{
+    [self.beaconManager stopRangingBeaconsInRegion:self.region];
+    self.beaconQuantityLabel.text = @(self.beaconList.count).description;
+    self.beaconList = beacons;
+    [self.beaconListTableView reloadData];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.beaconList.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *beaconCell = [tableView dequeueReusableCellWithIdentifier:@"BeaconCell"];
+
+    ESTBeacon *curBeacon = self.beaconList[indexPath.row];
+    beaconCell.textLabel.text = curBeacon.major.description;
+    beaconCell.detailTextLabel.text = curBeacon.macAddress;
+
+    return beaconCell;
+}
 
 @end
